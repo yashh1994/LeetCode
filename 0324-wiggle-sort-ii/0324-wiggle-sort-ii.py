f9f8842f4e402dc6f59464f8697668bf663d4be7
YASH FADADU <137137833+yash-1994@users.noreply.github.com>
2024-09-01 20:52:11 +0530

    Time: 118 ms (89.96%), Space: 19.3 MB (81.04%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -4,8 +4,5 @@ class Solution:
     def wiggleSort(self, nums: List[int]) -> None:
         nums.sort()
         mid = (len(nums) + 1) // 2
-        left = nums[:mid][::-1]
-        right = nums[mid:][::-1]
-
-        for i in range(len(nums)):
-            nums[i] = left.pop(0) if i % 2 == 0 else right.pop(0)
+        nums[::2],nums[1::2] = nums[:mid][::-1],nums[mid:][::-1]
+        
