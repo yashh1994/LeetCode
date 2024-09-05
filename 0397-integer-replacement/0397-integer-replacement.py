a316bcf85232c1c70a6b187f4e314cf03978ff44
YASH FADADU <137137833+yash-1994@users.noreply.github.com>
2024-09-05 19:16:30 +0530

    Time: 32 ms (82.97%), Space: 16.5 MB (58.15%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.py
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -1,18 +1,14 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
-        def go(n: int, count: int):
-            nonlocal ans  
-            if n == 1:
-                ans = min(ans, count)
-                return
-            if n <= 0 or ans < count:
-                return
+        ans = 0
+        while n > 1:
             if n % 2 == 0:
-                go(n // 2, count + 1)
+                n >>= 1
             else:
-                go(n + 1, count + 1)
-                go(n - 1, count + 1)
-
-        ans = float('inf')  # Initialize ans to a large value
-        go(n, 0)
+                if n == 3 or (n & 2) == 0:
+                    n -= 1
+                else:
+                    n += 1
+            ans += 1
         return ans
+        
