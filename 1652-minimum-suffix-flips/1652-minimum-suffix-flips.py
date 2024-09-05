c269351f4968d0ba94ab2c2d6b2aabd7e4d2bef3
YASH FADADU <137137833+yash-1994@users.noreply.github.com>
2024-09-05 21:57:19 +0530

    Time: 60 ms (60.73%), Space: 17.3 MB (36.91%) - LeetHub

--- 1652-minimum-suffix-flips/1652-minimum-suffix-flips.py
+++ 1652-minimum-suffix-flips/1652-minimum-suffix-flips.py
@@ -1,20 +1,5 @@
 class Solution:
     def minFlips(self, target: str) -> int:
-        # def swap(s,i) -> str:
-        #     ans = s[:i]
-        #     for k in range(i,len(s)):
-        #         ans += '1' if s[k] == '0' else '0'
-        #     return ans
-        # ans = "0"*len(target)
-        # ope = 0
-        # for i in range(len(ans)):
-        #     c = ans[i]
-        #     if ans == target:
-        #         return ope
-        #     if c != target[i]:
-        #         ans = swap(ans,i)
-        #         ope += 1
-        # return ope
         ans = 0
         ct = 0
 
