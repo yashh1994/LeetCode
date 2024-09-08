20d80a745177744386443e114048bc681edc8641
YASH FADADU <137137833+yash-1994@users.noreply.github.com>
2024-09-08 12:38:14 +0530

    Time: 316 ms (8.68%), Space: 19 MB (48.84%) - LeetHub

--- 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
@@ -1,29 +1,16 @@
-from collections import Counter
+from typing import List
 
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        def com_str(s1:str,s2:str) -> bool:
-            i1,i2 = 0,0
-            print(f'Checking {s1} {s2}')
-            while i1<len(s1) and i2<len(s2):
+        def com_str(s1: str, s2: str) -> bool:
+            i1, i2 = 0, 0
+            while i1 < len(s1) and i2 < len(s2):
                 if s1[i1] == s2[i2]:
-                    i1 += 1
                     i2 += 1
-                else:
-                    i1 += 1
-            print(i1,' ',i2)
-            if i2 < len(s2):
-                return False
-            return True
-
-        ans = ""
-        for st in dictionary:
-            if com_str(s,st):
-                if not ans or len(ans) < len(st):
-                    ans = st
-                elif len(ans) == len(st) and st < ans:
-                    ans = st
-                 
-        return ans
+                i1 += 1
+            return i2 == len(s2)
         
+        valid_words = [word for word in dictionary if len(word) <= len(s) and com_str(s, word)]
         
+       
+        return min(valid_words, key=lambda word: (-len(word), word), default="")
